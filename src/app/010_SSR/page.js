531f128a1beab16540557863d5f9f96d9b0c79f6
daichi0918 <kayasugadaichi@icloud.com>
2024-11-06 06:46:58 +0900

    commit

--- src/app/010_SSR/page.js
+++ src/app/010_SSR/page.js
@@ -3,7 +3,8 @@ import ClientComp from './components/ClientComp';
 import { ENDPOINT } from '@/constants';
 import ArticleList from '@/components/articleList';
 
-export default async function SSR() {
+export default async function SSR({ params }) {
+  console.log(params);
   const articles = await fetch(ENDPOINT, {
     next: { revalidate: 10 },
   }).then((res) => res.json());
