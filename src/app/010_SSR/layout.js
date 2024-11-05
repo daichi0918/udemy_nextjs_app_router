e7a5d57f1789898702831879ea2d161c8217d8fe
daichi0918 <kayasugadaichi@icloud.com>
2024-11-05 16:01:23 +0900

    finish:chapter190

--- src/app/010_SSR/layout.js
+++ src/app/010_SSR/layout.js
@@ -1,3 +1,3 @@
 export default function subLayout({ children }) {
-  return <div style={{ background: 'red' }}>{children}</div>;
+  return <div>{children}</div>;
 }
--- src/app/010_SSR/page.js
+++ src/app/010_SSR/page.js
@@ -1,10 +1,14 @@
+import { ENDPOINT } from '@/constants';
 import ClientComp from './components/ClientComp';
+import ArticleList from '@/components/articleList';
 
-export default function SSR() {
+export default async function SSR() {
+  const articles = await fetch(ENDPOINT).then((res) => res.json());
   return (
     <>
       <div>SSR Page</div>
       <ClientComp />
+      <ArticleList list={articles} bathPath="/010_SSR" />
     </>
   );
 }
