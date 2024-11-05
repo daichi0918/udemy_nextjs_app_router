7f878282ac95bc1788d3c91abcdce16832b00afe
daichi0918 <kayasugadaichi@icloud.com>
2024-11-05 16:18:36 +0900

    finish:chapter191

--- src/app/010_SSR/page.js
+++ src/app/010_SSR/page.js
@@ -1,14 +1,17 @@
-import { ENDPOINT } from '@/constants';
+import './lib';
 import ClientComp from './components/ClientComp';
+import { ENDPOINT } from '@/constants';
 import ArticleList from '@/components/articleList';
 
 export default async function SSR() {
-  const articles = await fetch(ENDPOINT).then((res) => res.json());
+  const articles = await fetch(ENDPOINT, {
+    next: { revalidate: 10 },
+  }).then((res) => res.json());
   return (
     <>
       <div>SSR Page</div>
       <ClientComp />
-      <ArticleList list={articles} bathPath="/010_SSR" />
+      <ArticleList list={articles} basePath="/010_SSR" />
     </>
   );
 }
